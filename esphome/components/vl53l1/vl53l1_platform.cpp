64a5da4ad5ccb259ba3518c9c9a7643c68fe9a6e
Stian V. Svedenborg <deinoff@gmail.com>
2025-09-30 22:10:27 +0200

    Integration-issues

--- esphome/components/vl53l1/vl53l1_platform.cpp
+++ esphome/components/vl53l1/vl53l1_platform.cpp
@@ -70,11 +70,11 @@
   int8_t VL53L1_RdByte(uint16_t dev, uint16_t index, uint8_t *data) {
     uint8_t status = VL53L1X_ERROR_TIMEOUT;
 
-    if(lookup(dev)->read_byte(index, pdata)) {
+    if(lookup(dev)->read_byte(index, data)) {
       status = VL53L1X_ERROR_NONE;
     }
     
-    ESP_LOGD("platform", "READ byte: %s", count, esphome::format_hex(pdata, count).c_str());
+    ESP_LOGD("platform", "READ byte: %s", count, esphome::format_hex(data, count).c_str());
     
     return status;
   }
