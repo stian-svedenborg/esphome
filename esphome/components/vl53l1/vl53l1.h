66b2e99dc850c95e1d57d860d30f72f4bd7ee044
Stian V. Svedenborg <deinoff@gmail.com>
2025-09-30 20:37:50 +0200

    Integration-issues

--- esphome/components/vl53l1/vl53l1.h
+++ esphome/components/vl53l1/vl53l1.h
@@ -21,10 +21,10 @@ class VL53L1Sensor : public sensor::Sensor, public PollingComponent, public i2c:
   void set_timing_budget(uint32_t timing_budget) { this->measurement_timing_budget_us_ = timing_budget; }
   void set_distance_mode(DistanceMode mode) { this->distance_mode_ = mode; }
 
-  float get_setup_priority() const {
+  float get_setup_priority() const override {
     // Return the setup priority of this component
     // Higher values mean this component will be set up later
-    return setup_priority::DATA;
+    return setup_priority::LATE;
   }
 
  protected:
