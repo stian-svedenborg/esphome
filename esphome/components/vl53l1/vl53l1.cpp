8db0faa974519e0958cace5f9df7fb61ffda98b8
Stian V. Svedenborg <deinoff@gmail.com>
2025-09-30 13:03:04 +0200

    Integration-issues

--- esphome/components/vl53l1/vl53l1.cpp
+++ esphome/components/vl53l1/vl53l1.cpp
@@ -1,11 +1,18 @@
 #include "vl53l1.h"
-
+#include "esphome/components/i2c/i2c.h"
 #include "esphome/core/log.h"
 
 extern "C" {
 #include "VL53L1X_api.h"
 }
-#include "vl53l1_platform.h" 
+
+namespace esphome {
+namespace vl53l1_platform_bridge {
+// Set the current I2C device instance for the vendor VL53L1 ULD C API bridge
+void set_current_device(esphome::i2c::I2CDevice *dev);
+}  // namespace vl53l1_platform_bridge
+}  // namespace esphome
+
 
 namespace esphome {
 namespace vl53l1 {
