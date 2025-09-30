b2d44247e50cad18526d74eb734125b1f4d12ded
Stian V. Svedenborg <deinoff@gmail.com>
2025-09-30 21:44:58 +0200

    Integration-issues

--- esphome/components/vl53l1/vl53l1_platform.cpp
+++ esphome/components/vl53l1/vl53l1_platform.cpp
@@ -52,27 +52,27 @@
   }
   
   int8_t VL53L1_WrByte(uint16_t dev, uint16_t index, uint8_t data) {
-      return VL53L1_WriteMulti(dev, index, std::reinterpret_cast<uint8_t*>(&data), sizeof(data));
+      return VL53L1_WriteMulti(dev, index, reinterpret_cast<uint8_t*>(&data), sizeof(data));
   }
   
   int8_t VL53L1_WrWord(uint16_t dev, uint16_t index, uint16_t data) {
-      return VL53L1_WriteMulti(dev, index, std::reinterpret_cast<uint8_t*>(&data), sizeof(data));
+      return VL53L1_WriteMulti(dev, index, reinterpret_cast<uint8_t*>(&data), sizeof(data));
   }
   
   int8_t VL53L1_WrDWord(uint16_t dev, uint16_t index, uint32_t data) {
-    return VL53L1_WriteMulti(dev, index, std::reinterpret_cast<uint8_t*>(&data), sizeof(data));
+    return VL53L1_WriteMulti(dev, index, reinterpret_cast<uint8_t*>(&data), sizeof(data));
   }
   
   int8_t VL53L1_RdByte(uint16_t dev, uint16_t index, uint8_t *data) {
-    return VL53L1_ReadMulti(dev, index, std::reinterpret_cast<uint8_t*>(&data), sizeof(*data));
+    return VL53L1_ReadMulti(dev, index, reinterpret_cast<uint8_t*>(&data), sizeof(*data));
   }
   
   int8_t VL53L1_RdWord(uint16_t dev, uint16_t index, uint16_t *data) {
-    return VL53L1_ReadMulti(dev, index, std::reinterpret_cast<uint8_t*>(&data), sizeof(*data));
+    return VL53L1_ReadMulti(dev, index, reinterpret_cast<uint8_t*>(&data), sizeof(*data));
   }
   
   int8_t VL53L1_RdDWord(uint16_t dev, uint16_t index, uint32_t *data) {
-    return VL53L1_ReadMulti(dev, index, std::reinterpret_cast<uint8_t*>(&data), sizeof(*data));
+    return VL53L1_ReadMulti(dev, index, reinterpret_cast<uint8_t*>(&data), sizeof(*data));
   }
   
   int8_t VL53L1_WaitMs(uint16_t dev, int32_t wait_ms){
