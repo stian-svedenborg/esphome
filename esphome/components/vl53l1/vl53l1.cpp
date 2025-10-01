86e1f6fe8ad716b653fa56944233ee490e28fcbb
Stian V. Svedenborg <deinoff@gmail.com>
2025-10-01 14:21:33 +0200

    Testing

--- esphome/components/vl53l1/vl53l1.cpp
+++ esphome/components/vl53l1/vl53l1.cpp
@@ -25,7 +25,7 @@ void VL53L1Sensor::setup() {
   esphome::delay(3);
   register_sensor(this); // Bridge I2C for vendor API
   if ((err = VL53L1X_SetI2CAddress(DEFAULT_I2C_ADDRESS, this->get_i2c_address())) == VL53L1X_ERROR_NONE) {
-    ESP_LOGW(TAG, "StartRanging failed: %d", err);
+    ESP_LOGW(TAG, "SetI2CAddress failed: %d", err);
     this->mark_failed();
     return;
   }
