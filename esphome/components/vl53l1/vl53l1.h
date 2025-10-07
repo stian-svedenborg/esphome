c01e3b0e9bef4b27febaad1a50a7a0440f723edf
Stian V. Svedenborg <deinoff@gmail.com>
2025-10-07 21:52:08 +0200

    Working

--- esphome/components/vl53l1/vl53l1.h
+++ esphome/components/vl53l1/vl53l1.h
@@ -32,8 +32,8 @@ class VL53L1Sensor : public sensor::Sensor, public Component, public i2c::I2CDev
   void set_distance_mode(DistanceMode mode) { this->distance_mode_ = mode; }
   void set_update_interval(uint32_t update_interval_ms) { this->update_interval_ms_ = update_interval_ms; }
 
-  void schedule_update_from_isr() {
-    this->defer([this](){this->update();});
+  static void schedule_update_from_isr(VL53L1Sensor *sensor) {
+    sensor->defer([sensor](){sensor->update();});
   }
 
  protected:
